220bc0cadfc968a9c0da3086e83b1707a6314f92
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-01-20 11:57:11 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str  = sc.nextLine();
+		System.out.println((int)str.charAt(0));
+		
+	
+	}
+	
+	
+}
