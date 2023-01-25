30a2eb5a3b48b999d2fdb1f473d563dd9c7ac7db
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-01-25 12:47:49 +0900

    [Silver IV] Title: 덱, Time: 408 ms, Memory: 20572 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		LinkedList <Integer> q = new LinkedList<>();
+		
+		for(int i =0;i<N;i++) {
+			String str= br.readLine();
+			String[] temp = str.split(" ");
+			if(temp[0].equals("push_front")) {
+				q.addFirst(Integer.parseInt(temp[1]));
+			}
+			else if(temp[0].equals("push_back")) {
+				q.addLast(Integer.parseInt(temp[1]));
+			}
+			else if(temp[0].equals("pop_front"))
+				if(q.isEmpty())
+					System.out.println(-1);
+				else
+					System.out.println(q.pollFirst());
+			else if(temp[0].equals("pop_back"))
+				if(q.isEmpty())
+					System.out.println(-1);
+				else
+					System.out.println(q.pollLast());
+			else if(temp[0].equals("size"))
+				System.out.println(q.size());
+			else if(temp[0].equals("empty"))
+				if(q.isEmpty())
+					System.out.println(1);
+				else	
+					System.out.println(0);
+			else if(temp[0].equals("front")) {
+				if(q.isEmpty())
+					System.out.println(-1);
+				else	
+					System.out.println(q.getFirst());
+			}
+			else if(temp[0].equals("back")) {
+				if(q.isEmpty())
+					System.out.println(-1);
+				else	
+					System.out.println(q.getLast());
+			}
+			
+		}
+	}
+	
+	
+	
+}
