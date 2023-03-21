521805ad66b61fe30010d8673f6a2e0a82934fb7
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-03-21 16:27:22 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(cards1, cards2, goal):
+    c1 = deque(cards1)
+    c2 = deque(cards2)
+    
+
+    for char in goal:
+        if c1 and char == c1[0]:
+            c1.popleft()
+        elif c2 and char == c2[0]:
+            c2.popleft()
+        else:
+            return "No"
+    
+    return "Yes"
