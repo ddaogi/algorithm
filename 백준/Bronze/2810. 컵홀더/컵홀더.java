73b364dc6863e62ca7f7cbf10d4930c60e8beeb7
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-03-27 18:44:22 +0900

    [Bronze I] Title: 컵홀더, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2810. 컵홀더/컵홀더.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		int count = 1;
+		for(int i=0; i<num; i++) {
+			if(str.charAt(i) == 'S') count++;
+			else if(str.charAt(i) == 'L') {
+				count += 1;
+				i++;
+				}
+			}
+		if(num > count) {
+			System.out.println(count);
+		}
+		else {
+			System.out.println(num);
+		}
+	}
+}
