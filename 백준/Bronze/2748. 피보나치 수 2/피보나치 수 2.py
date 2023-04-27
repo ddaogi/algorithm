9219d9a4991a471822245ef3918db9668320864c
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-04-27 23:58:03 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n+1)
+for i in range(n+1):
+    if i == 1 or i ==2:
+        dp[i] = 1
+    else:
+        dp[i] = dp[i-1]+dp[i-2]
+
+print(dp[n])
