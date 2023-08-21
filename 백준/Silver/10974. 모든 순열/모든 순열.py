d78d7c9eeefdfb7d03f3ad526a0bfb4af511963b
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-08-21 13:41:11 +0900

    [Silver III] Title: 모든 순열, Time: 112 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -0,0 +1,15 @@
+def dfs(depth, n, acc_str):
+    if depth == n:
+        print(acc_str.strip())  # Remove leading space
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            new_str = acc_str + ' ' + str(i+1)
+            dfs(depth + 1, n, new_str)
+            visited[i] = False
+
+n = int(input())
+visited = [False] * n
+dfs(0, n, '')
