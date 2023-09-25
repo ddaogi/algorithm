8350f9067927b1094b6e14f5d07b696457c2ea36
ddaogi <47049582+ddaogi@users.noreply.github.com>
2023-09-25 21:09:47 +0900

    [level 2] Title: 괄호 변환, Time: 7.99 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,42 @@
+def isbalanced(s):
+	value=0
+	for c in s:
+		if c=='(': value+=1
+		elif c==')': value-=1
+
+	if value==0: return True
+	else: return False
+
+def iscorrect(s):
+	stack=[]
+	stack.append(s[0])
+	for i in range(1,len(s)):
+		if len(stack)==0 or stack[-1]==')' or (stack[-1]=='(' and s[i]=='('):
+			stack.append(s[i])
+		else:
+			stack.pop()
+	if len(stack)==0: return True
+	else: return False
+
+
+def solution(p):
+	answer = ''
+	u=""
+	v=""
+	if len(p)==0 or iscorrect(p): return p
+
+	for i in range(2,len(p)+1,2):
+		if isbalanced(p[0:i]):
+			u=p[0:i]
+			v=p[i:len(p)]
+			break
+
+	if iscorrect(u):
+		answer+=u+solution(v)
+	else:
+		answer+='('+solution(v)+')'
+		for c in u[1:-1]:
+			if c=='(': answer+=')'
+			else: answer+='('
+
+	return answer
